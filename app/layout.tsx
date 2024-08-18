b7b225dfd2ef46e31e422108f12045958d68b919
rohit27-2 <gaddamrohitgoud@gmail.com>
2024-08-18 15:17:41 +0530

    Auth Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/header";
-
-// import { Jomhuria } from "next/font/google";
-// const jomhuria = Jomhuria({
-//   subsets: ["latin"],
-//   weight: "400",
-// });
+import Navigation from "./_components/navigation";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,12 +31,15 @@ export default function RootLayout({
       <body
         className={`${inter.className}  bg-primary-100 text-black min-h-screen flex flex-col`}
       >
-        <Header />
-        <div className="flex-1 px-4 py-4 grid">
-          <main className="mx-auto w-full max-w-7xl overflow-x-hidden">
-            {children}
-          </main>
-        </div>
+        <ClerkProvider>
+          <Header />
+          
+          <div className="flex-1 px-4 py-4 grid">
+            <main className="mx-auto w-full max-w-7xl overflow-x-hidden">
+              {children}
+            </main>
+          </div>
+        </ClerkProvider>
       </body>
     </html>
   );
