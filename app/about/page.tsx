59bb324bbe9fc81823c1619954dc0a2059d3eaa2
Favour Oghenekowho <gh.oghene@gmail.com>
2024-09-10 23:25:48 +0100

    updated about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,9 +2,17 @@ import React from "react";
 
 export default function About() {
   return (
-    <div>
-      AptSense is an AI-powered application designed to help students excel in
-      their aptitude examinations.
+    <div className="mx-auto px-6">
+      AptSense is a cutting-edge, AI-powered application created with the
+      primary goal of empowering students to succeed in their aptitude
+      examinations. By leveraging advanced machine learning algorithms, AptSense
+      provides personalized study materials, practice tests, and real-time
+      feedback to help students identify their strengths and areas for
+      improvement. The platform is designed to adapt to individual learning
+      styles, offering a seamless and interactive experience that enhances
+      learning efficiency. Whether preparing for competitive exams or personal
+      skill development, AptSense equips students with the tools they need to
+      excel and achieve their academic goals with confidence.
     </div>
   );
 }
